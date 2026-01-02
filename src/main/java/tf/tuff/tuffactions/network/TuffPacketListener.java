550ca5113796288f9bc56d15bdcbfd645d8616e2
Potato. <105754685+dragon731012@users.noreply.github.com>
2026-01-02 12:33:57 -1000

    Add files via upload

--- src/main/java/tf/tuff/tuffactions/network/TuffPacketListener.java
+++ src/main/java/tf/tuff/tuffactions/network/TuffPacketListener.java
@@ -1,1 +1,42 @@
+package net.potato.tuff.network;
 
+import com.github.retrooper.packetevents.event.PacketListenerAbstract;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPluginMessage;
+import net.potato.tuff.TuffActions;
+import org.bukkit.entity.Player;
+
+public class TuffPacketListener extends PacketListenerAbstract {
+    private final TuffActions plugin;
+
+    public TuffPacketListener(TuffActions plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (event.getPacketType() != PacketType.Play.Client.PLUGIN_MESSAGE) {
+            return;
+        }
+
+        Object rawPlayer = event.getPlayer();
+        if (!(rawPlayer instanceof Player)) {
+            return;
+        }
+
+        WrapperPlayClientPluginMessage wrapper = new WrapperPlayClientPluginMessage(event);
+        wrapper.read();
+
+        if (!TuffActions.CHANNEL.equals(wrapper.getChannelName())) {
+            return;
+        }
+
+        byte[] data = wrapper.getData();
+        if (data == null || data.length == 0) {
+            return;
+        }
+
+        plugin.handlePluginMessage((Player) rawPlayer, data);
+    }
+}
